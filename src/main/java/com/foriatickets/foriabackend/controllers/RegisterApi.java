88db17c1d348516f1bdc2de5c224f7f98ec9f6bd
Corbin Schwalm <corbin@foriatickets.com>
2019-06-12 23:50:16 -0700

    FB-14: Modify Create User Logic
    Corrected API controller mapping.

---
+++ src/main/java/com/foriatickets/foriabackend/controllers/RegisterApi.java
@@ -0,0 +1,35 @@
+package com.foriatickets.foriabackend.controllers;
+
+import com.foriatickets.foriabackend.service.UserCreationService;
+import io.swagger.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(path = "/v1/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+public class RegisterApi implements io.swagger.api.RegisterApi {
+
+    private UserCreationService userCreationService;
+
+    public RegisterApi(@Autowired UserCreationService userCreationService) {
+
+        assert userCreationService != null;
+        this.userCreationService = userCreationService;
+    }
+
+    @Override
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<User> registerUser(@Valid @RequestBody User body) {
+
+        User newUser = userCreationService.createUser(body);
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+}
--- src/main/java/com/foriatickets/foriabackend/controllers/UserApi.java
+++ src/main/java/com/foriatickets/foriabackend/controllers/UserApi.java
@@ -1,41 +1,21 @@
 package com.foriatickets.foriabackend.controllers;
 
-import com.foriatickets.foriabackend.service.UserCreationService;
 import io.swagger.model.Ticket;
 import io.swagger.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Size;
 import java.util.List;
 
 @Controller
 @RequestMapping(path = "/v1/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserApi implements io.swagger.api.UserApi {
-
-    private UserCreationService userCreationService;
-
-    public UserApi(@Autowired UserCreationService userCreationService) {
-
-        assert userCreationService != null;
-        this.userCreationService = userCreationService;
-    }
-
-    @Override
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<User> createUser(@Valid @RequestBody User body) {
-
-        User newUser = userCreationService.createUser(body);
-        return new ResponseEntity<>(newUser, HttpStatus.OK);
-    }
 
     @Override
     @RequestMapping(value = "/user/{user_id}/tickets", method = RequestMethod.GET)
