e3d1e615d72eb7d0caef3d9b831ff49ba80d87d3
Corbin Schwalm <corbin@foriatickets.com>
2019-07-17 09:46:02 -0700

    Modified User API Scope to logged in user.

--- src/main/java/com/foriatickets/foriabackend/controllers/UserApi.java
+++ src/main/java/com/foriatickets/foriabackend/controllers/UserApi.java
@@ -6,26 +6,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
-import java.util.UUID;
 
 @Controller
 @RequestMapping(path = "/v1/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserApi implements org.openapitools.api.UserApi {
 
     @Override
-    @RequestMapping(value = "/user/{user_id}/tickets", method = RequestMethod.GET)
-    public ResponseEntity<List<Ticket>> getTickets(@PathVariable("user_id") UUID userId) {
+    @RequestMapping(value = "/user/tickets", method = RequestMethod.GET)
+    public ResponseEntity<List<Ticket>> getTickets() {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @Override
-    @RequestMapping(value = "/user/{user_id}", method = RequestMethod.GET)
-    public ResponseEntity<User> getUser(@PathVariable("user_id") UUID userId) {
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity<User> getUser() {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
